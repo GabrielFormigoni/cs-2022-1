d8fb9699a81d0e0adaee9eed0993deb84c58df90
Gabriel Formigoni <bielformigoni@gmail.com>
2022-07-06 19:38:31 -0300

    Adicionando a Tarefa10

---
+++ tarefa010/ex02/Agenda.java
@@ -0,0 +1,63 @@
+package ex02;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Agenda {
+    
+    private ArrayList<Contato> contatos = new ArrayList<Contato>();
+
+
+
+    public ArrayList<Contato> getContatos() {
+        return this.contatos;
+    }
+
+    public void setContatos(ArrayList<Contato> contatos) {
+        this.contatos = contatos;
+    }
+
+    public void addContatos(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public void inclusaoContato() {
+        String nome = JOptionPane.showInputDialog(null, "Informe o nome do contato.");
+        String email = JOptionPane.showInputDialog(null, "Informe o email do contato.");
+
+        contatos.add(new Contato(nome, email));
+    }
+
+    public void exclusaoContato() {
+        String nome = JOptionPane.showInputDialog(null, "Informe o nome do contato que será excluído ");
+
+        for (int i = 0; i < contatos.size(); i++) {
+            if (contatos.get(i).getNome().equals(nome)) {
+
+                contatos.remove(contatos.get(i));
+                JOptionPane.showMessageDialog(null, "Contato " + nome + " excluído !!!");
+            }
+        }
+    }
+
+    public void consultaContato() {
+        String nome = JOptionPane.showInputDialog(null, "Informe o nome do contato que será consultado ");
+
+        for (int i = 0; i < contatos.size(); i++) {
+            if (contatos.get(i).getNome().equals(nome)) {
+                JOptionPane.showMessageDialog(null, contatos.get(i));
+            }
+        }
+    }
+
+    public void listaContatos() {
+        String retorno = "";
+
+        for (int i = 0; i < contatos.size(); i++) {
+            retorno = retorno + "Contato " + (i + 1) + " : \n" + contatos.get(i).consulta();
+        }
+
+        JOptionPane.showMessageDialog(null, retorno);
+    }
+
+}
---
+++ tarefa010/ex02/AgendaTeste.java
@@ -0,0 +1,13 @@
+package ex02;
+
+public class AgendaTeste {
+    
+    public static void main(String[] args)  {
+
+        Contato contato1 = new Contato("Adriana Oliveira", "antonio@ufg.br");
+        Contato contato2 = new Contato("Antonio Marco", "antonio.marcos@ufg.br");
+        Contato contato3 = new Contato("Jorley", "jorley@ufg.br");
+
+
+    }
+}
